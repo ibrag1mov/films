852f9bbb33ac9c1d1f9dd9086b78f73b51dfe6b8
ibrag1mov <ibragimov.abdulhay@mail.ru>
2022-12-21 12:20:15 +0500

    sort add

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ function newcards (array, node){
                 <img class="card-img" src="${film.poster}" alt="${film.title}">
             </div>
             <h2 class="text-center my-3 text-white fs-4">${film.title}</h2>
+            <p class="text-center text-info">${film.genres}</p>
             <div class=" card-text">
                 <p class="fs-6 text-white  text">
                 ${film.overview}
@@ -53,6 +54,7 @@ elFrom.addEventListener('input',(evt)=>{
     newArray=[]
 });
 
+// select optin 
 
 let elSelect=$('#genres');
 
@@ -77,7 +79,7 @@ for(i of arr){
 let newArr=[];
 elSelect.addEventListener('change',(e)=>{
     newArr=[]
-
+    
 if(elSelect.value!=="All"){
     films.forEach((film)=>{
         if(film.genres.includes(elSelect.value)){
@@ -88,5 +90,29 @@ if(elSelect.value!=="All"){
 }else{
     newcards(films, cardWrapper)
 }
+e.preventDefault()
     
+})
+
+// sort
+let elSelect2=$('#sort');
+
+
+elSelect2.addEventListener('change',(e)=>{
+    let elSelect2Val=elSelect2.value;
+    if(elSelect2Val != "Sort"){
+        if(elSelect2Val == "A-Z"){
+            const filmSort = films.sort((a, b)=> a.title.charCodeAt(0)-b.title.charCodeAt(0));
+            newcards(filmSort, cardWrapper);
+        }
+        else if(elSelect2Val == "Z-A"){
+            const filmSort2 = films.sort((a, b)=> b.title.charCodeAt(0)-a.title.charCodeAt(0));
+            newcards(filmSort2, cardWrapper);
+        }
+    }
+    else{
+        window.location.reload();
+    }
+    
+    e.preventDefault();
 })
